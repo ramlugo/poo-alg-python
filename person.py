f78e1ee5c23d07f76cdc5f807156de76d11f06f9
Ram Lugo <ramlugo2020@gmail.com>
2020-12-17 19:19:20 -0600

    Creamos la clase Persona y su método saludar

--- person.py
+++ person.py
@@ -1,2 +1,20 @@
+def run():
+    # Definimos una clase "Persona"
+    class Persona:
+
+        def __init__(self, nombre, edad):
+            self.nombre = nombre
+            self.edad = edad
+
+        def saludar(self, otra_persona):
+            return f'Hola {otra_persona.nombre}, me llamo {self.nombre}'
+
+
+    # Así se usa nuestra clase persona
+    david = Persona('David', 35)
+    ramses = Persona('Ramses', 21)
+
+    david.saludar(ramses)
+
 if __name__ == "__main__":
-    pass
+    run()
