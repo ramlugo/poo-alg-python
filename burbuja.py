abb8a96428ed363e1591ea410527dfa503760ab4
Ram Lugo <ramlugo2020@gmail.com>
2021-01-12 19:57:19 -0600

    Complejidad algorítmica resuelta

--- burbuja.py
+++ burbuja.py
@@ -5,7 +5,7 @@ def ordenamiento_burbuja(lista):
     n = len(lista)
 
     for i in range(n):
-        for j in range(0, n - i - 1):
+        for j in range(0, n - i - 1): # O(n) * O(n) = O(n**2)
 
             if lista[j] > lista[j + 1]:
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
